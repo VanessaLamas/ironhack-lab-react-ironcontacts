94b3f091bb3ad98650d49ffd025462480ff2d189
John Doe <johndoe@example.com>
2023-06-05 13:45:30 +0200

    pending iteration 3-4-5

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import contacts from "./contacts.json";
 import Contacts from "./components/Contacts";
-
 function App() {
   return (
     <div className="App">
       <h1>IronContacts</h1>
-           {/* <List /> */}
-           <Contacts />
+      {/* <List /> */}
+      <Contacts />
     </div>
   );
 }
-
 export default App;
+
+
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,57 +1,42 @@
 import React from "react";
+import contacts from "../contacts.json";
 function Contacts() {
-    const contacts =
-        [
-            {
-                "name": "Idris Elba",
-                "pictureUrl": "https://image.tmdb.org/t/p/w500/d9NkfCwczP0TjgrjpF94jF67SK8.jpg",
-                "popularity": 11.622713,
-                "id": "11731993-0604-4bee-80d5-67ad845d0a38",
-                "wonOscar": false,
-                "wonEmmy": false
-            },
-            {
-                "name": "Johnny Depp",
-                "pictureUrl": "https://image.tmdb.org/t/p/w500/kbWValANhZI8rbWZXximXuMN4UN.jpg",
-                "popularity": 15.656534,
-                "id": "7dad00f7-3949-477d-a7e2-1467fd2cfc06",
-                "wonOscar": false,
-                "wonEmmy": false
-            },
-            {
-                "name": "Monica Bellucci",
-                "pictureUrl": "https://image.tmdb.org/t/p/w500/qlT4904d8oi2NIs28RrgnIZDFZB.jpg",
-                "popularity": 16.096436,
-                "id": "0ad5e441-3084-47a1-9e9b-b917539bba71",
-                "wonOscar": false,
-                "wonEmmy": false
-            },
-            {
-                "name": "Gal Gadot",
-                "pictureUrl": "https://image.tmdb.org/t/p/w500/34kHAyBaBhq2kUrmhM15paEBuuI.jpg",
-                "popularity": 10.049256,
-                "id": "b497e3c4-50bb-4ae2-912f-eb36802c5bc2",
-                "wonOscar": false,
-                "wonEmmy": false
-            },
-            {
-                "name": "Ian McKellen",
-                "pictureUrl": "https://image.tmdb.org/t/p/w500/coWjgMEYJjk2OrNddlXCBm8EIr3.jpg",
-                "popularity": 10.070132,
-                "id": "0067ae32-97b6-4431-898e-eb1c10150abb",
-                "wonOscar": false,
-                "wonEmmy": false
-            }
-        ]
+    const maxContactsToShow = 5; 
+    const rowsStyle = {
+        margin: '10px 40px',
+        display: 'flex',
+        width: '100px'
+    };
+    const pictureStyle = {
+        margin: '10px 40px',
+        height: '100px',
+        width: '130px'
+    };
+    const iconStyle = {
+        margin: '10px 40px',
+        height: '50px',
+        width: '50px'
+    };
     return (
         <div className="List">
-            {contacts.map(contact => (
-                <div key={contact.id}>
-                    <h3>{contact.name}</h3>
-                    <h5>{contact.popularity}</h5>
-                    <img src={contact.pictureUrl} alt={contact.name} />
-                </div>
-            ))}
+            <table>
+                <tr>
+                    <td> <h3 style={rowsStyle}>Picture</h3></td>
+                    <td> <h3 style={rowsStyle}>Name</h3></td>
+                    <td> <h3 style={rowsStyle}>Popularity</h3></td>
+                    <td> <h3 style={rowsStyle}>wonOscar</h3></td>
+                    <td> <h3 style={rowsStyle}>wonEmmy</h3></td>
+                </tr>
+                {contacts.slice(0, maxContactsToShow).map(contact => (
+                    <tr key={contact.id}>
+                        <td><img style={pictureStyle} src={contact.pictureUrl} alt={contact.name} /></td>
+                        <td> <p style={rowsStyle}>{contact.name}</p></td>
+                        <td> <p style={rowsStyle}>{contact.popularity}</p></td>
+                        <td>{contact.wonOscar && <img style={iconStyle} src='https://cdn-icons-png.flaticon.com/512/2617/2617955.png' alt={contact.name} />}</td>
+                        <td>{contact.wonEmmy && <img style={iconStyle} src='https://cdn-icons-png.flaticon.com/512/2617/2617955.png' alt={contact.name} />}</td>
+                    </tr>
+                ))}
+            </table>
         </div>
     )
 }
